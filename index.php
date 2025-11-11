e358f72dc3f00be8ccc987e7b4d1db41153b07ad
Vinícius <viniciusstorch62@gmail.com>
2025-11-11 08:56:44 -0300

    Operadores aritméticos

--- index.php
+++ index.php
@@ -12,5 +12,12 @@
         $nome = "Romulo Beninca";
         echo $nome."<br>";
     ?>
+    <h2>Exemplo tipo de dados PHP</h2>
+    <?php
+        $a = 5;
+        $b = 7;
+        echo "<p>".$a+$b."</p>";
+    ?>
+    <p> <?php echo $a*$b ?></p>
 </body>
 </html>
