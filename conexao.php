afd9f7830948958b355019de765f916e5ba65b00
Vinícius <viniciusstorch62@gmail.com>
2025-11-18 11:15:25 -0300

    conexao.php

---
+++ conexao.php
@@ -0,0 +1,27 @@
+<?php
+    function conecta_bd() {
+        $servername = "localhost";
+        $username = "root";
+        $password = "admin";
+        $dbname = "webti";
+        
+        // Criar conexão
+        return new PDO("mysql:host=$servername; dbname=$dbname", $servername, $password);
+    }
+    // conecta_bd();
+
+    function cadastra_usuario($nome, $email, $login, $senha) {
+        $con = conecta_bd();
+        $stmt = $con->prepare("INSERT INTO usuarios (nome, email, login, senha)
+                                VALUES (:nome, :email, :login, :senha)");
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':login', $login);
+        $stmt->bindParam(':senha', $senha);
+        return $stmt->execute();
+
+    }
+
+    // Exemplo cadastro
+    cadastra_usuario("Dona Maria", "maria@email.com", "maria_dona", "123");
+?>
